0ecd7b4c195a47607299280acc4563a935016a36
Sparetire <s9ar371r3@gmail.com>
2017-03-20 22:04:23 +0800

    fix: bug fixed

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,8 @@
 function cors(opts) {
 	return function* (next) {
 		this.set('Access-Control-Allow-Origin', '*');
-		this.set('Access-Control-Request-Method', 'GET,POST,PUT,DELETE,OPTIONS');
-		this.set('Access-Control-Request-Headers', 'Content-Type,Content-LengthAccept,Accept-Charset,Accept-Encoding,Authorization,X-Requested-With,Token');
+		this.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+		this.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Accept,Accept-Charset,Accept-Encoding,Authorization,X-Requested-With,Token');
 		this.set('Access-Control-Max-Age', '7200');
 		yield next;
 	};
